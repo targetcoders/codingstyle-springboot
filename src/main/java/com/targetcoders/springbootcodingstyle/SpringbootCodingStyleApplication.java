a52a0f42524c9483cd6a182dcd9a87dbd6750827
greenneuron <goodev@kakao.com>
2024-10-11 10:33:37 +0900

    create project

---
+++ src/main/java/com/targetcoders/springbootcodingstyle/SpringbootCodingStyleApplication.java
@@ -0,0 +1,12 @@
+package com.targetcoders.springbootcodingstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCodingStyleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootCodingStyleApplication.class, args);
+  }
+}
---
+++ src/test/java/com/targetcoders/springbootcodingstyle/SpringbootCodingStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.targetcoders.springbootcodingstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCodingStyleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
